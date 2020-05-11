f0979d9c914971b2b3bcd26ea84e179de987b49e
sen1000-byte <nishiwaki.chihiro@gmail.com>
2020-05-11 20:26:35 +0900

    Initial Commit

---
+++ Count/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Count
+//
+//  Created by Chihiro Nishiwaki on 2020/05/11.
+//  Copyright © 2020 nishiwaki sen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
